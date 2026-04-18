b74062f038f7ea56aa2d91b5d2601d3daac2e491
Tx-Hydra <santii.faleroo@gmail.com>
2026-04-17 22:03:33 -0300

    Agenda.cpp

---
+++ Agenda.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <limits>
+#include <vector>
+
+
+struct tarea{
+    std::string tareas;
+    bool completada;
+};
+/*
+int main() {
+  // Create and open a text file
+  std::fstream MyFile("filename.txt");
+
+  // Write to the file
+  std::MyFile << "Files can be tricky, but it is fun enough!";
+  
+  // Read from the file
+  std::string myText;
+  getline(MyFile, myText);
+  std::cout << myText;
+
+  // Close the file
+  std::MyFile.close();
+} 
+*/
+void crearlista(){
+    std::ofstream MiLista("Tareas.txt", std::ios::app); //creo el archivo
+}
+
+tarea creartarea(){
+    tarea tar;
+    std::cout<<"Ingrese la tarea: ";
+    std::getline(std::cin >> std::ws ,tar.tareas);
+    tar.completada = false;
+    return tar;
+}
+
+void agregartarea(tarea tar){
+    std::ofstream agregar("Tareas.txt", std::ios::app);
+
+    if (!agregar.is_open()) {
+        std::cout << "Error al abrir archivo\n";
+        return;
+    }
+}
+
+
+void actualziartarea(){
+    std::ifstream archivo("Tareas.txt");
+    std::vector<tarea> lista;
+    std::string linea;
+
+
+    while (getline(archivo, linea)) {
+        tarea tar;
+        int pos = linea.find('|');
+
+        tar.tareas = linea.substr(0, pos);        
+        tar.completada = (linea[pos + 1] == '1');
+
+        lista.push_back(tar);
+    }
+    archivo.close();
+
+
+    int i;
+    std::cout << "Que tarea actualizar? ";
+    std::cin >> i;
+    if(lista[i].completada)
+        lista[i].completada = false;
+    else
+        lista[i].completada = true;
+
+
+    std::ofstream archivo2("Tareas.txt");
+    for (auto& t : lista) {
+        archivo2 << t.tareas << "|" << (t.completada ? 1 : 0) << "\n";
+    }
+}
+
+
+
+void vertareas() {
+    std::ifstream archivo("Tareas.txt");
+
+    if (!archivo.is_open()) {
+        std::cout << "Error al abrir el archivo\n";
+        return;
+    }
+
+    std::string linea;
+    int i = 0;
+
+    while (std::getline(archivo, linea)) {
+        tarea t;
+
+        int pos = linea.find('|');
+
+        t.tareas = linea.substr(0, pos);
+        t.completada = (linea[pos + 1] == '1');
+
+        std::cout << i << " - " << t.tareas 
+                  << " [" << (t.completada ? "si" : "no") << "]\n";
+
+        i++;
+    }
+}
+
+
+int main(){
+    tarea tar;
+    int opc = 1;
+    int opc2;
+    std::ifstream Lista;
+    crearlista();
+    while(opc != 2){
+        std::cout<<"** Agenda **"<< std::endl ;
+        std::cout<< std::endl <<"1- Abrir Agenda   2- Cerrar" << std::endl;
+        std::cout<<"R: ";
+        std::cin>>opc;
+        if (std::cin.fail()){
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cout<<"Opcion no Valida";
+            continue;
+        }
+        switch (opc){
+        case 1:
+            while (opc2 != 4){
+                std::ifstream Lista("Tareas.txt", std::ios::app);    
+                std::cout<<"1- Crear Tarea    2- Ver Tareas    3- Actualizar Tarea    4- Cerrar\n";
+                std::cout<<"R: ";
+                std::cin>>opc2;
+                if (std::cin.fail()){
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                    std::cout<<"Opcion no Valida";
+                    continue;
+                }
+                switch (opc2){
+                    case 1:
+                        tar = creartarea();
+                        agregartarea(tar);
+                        continue;;
+                    case 2:
+                        vertareas();
+                        break;
+                    case 3:
+                        actualziartarea();
+                        continue;
+                    case 4:
+                        opc = 2;
+                        break;
+                    default:
+                        continue;
+            
+                }
+            }
+        case 2:
+            break;
+        default:
+            std::cout<<"* Ingreso no Valido *\n\n";
+            continue;
+        }
+    }
+}
